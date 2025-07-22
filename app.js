d0ed5af5b4e900a0eadd1c85cf3122d7108fc827
Nidhi Kumari <nk.nidhi.7632@gmail.com>
2025-07-22 21:10:21 +0530

    mongo atlas added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError.js')
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -29,7 +30,7 @@ app.use(methodOverride('_method'))
 
 app.engine('ejs', ejsMate)
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
+const dbUrl = process.env.ATLASDB_URL;
 main()
     .then(() => {
         console.log('Connected to DB')
@@ -38,19 +39,34 @@ main()
         console.log('Database not connected', err)
     })
 async function main() {
-    await mongoose.connect(MONGO_URL)
+    await mongoose.connect(dbUrl)
 }
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto:{
+        secret:process.env.SECRET
+    }
+});
+
+store.on("error", (err)=>{
+    console.log("Error in MONGO SESSIONS STORE", err)
+})
+
 const sessionOptions={
-    secret: process.env.secret,
+    store: store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie:{
         expires:Date.now() + 7*24*60*60*1000 ,
         maxAge: 7*24*60*60*1000 ,
         httpOnly: true,
-    }
+    },
+    touchAfter: 24*60*60,
 };
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
